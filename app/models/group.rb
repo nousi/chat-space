5e9427f316f44685ca8a017859f7a5587ff7da14
nousi <nousidayo@gmail.com>
2019-08-11 22:00:23 +0900

    Revert "Merge pull request #4 from nousi/group機能実装2"
    This reverts commit 44ca286cb6d71805a565aaee6ae625a48bdb5ed5, reversing
    changes made to 7cbf6e6ee8667e8e6185d19d35917516b6520e6a.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,2 @@
 class Group < ApplicationRecord
-  has_many :members
-  has_many :users, through: :members
-  validates :name, presence: true, uniqueness: true
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,2 @@
 class Member < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
 end
