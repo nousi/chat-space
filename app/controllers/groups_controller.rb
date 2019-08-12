38f354bb80a30b4c0b18bd1af28ce7cdf66670f1
nousi <nousidayo@gmail.com>
2019-08-13 00:01:30 +0900

    Groupの編集機能のエラーを改善
    editアクションをupdateアクションに変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
+  def update
     if @group.update(group_params)
       redirect_to group_messages_path, notice: 'グループを編集しました'
     else
@@ -25,7 +25,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name,{ user_ids:[] })
+    params.require(:group).permit(:name,user_ids: [] )
   end
   def set_group
     @group = Group.find(params[:id])
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,6 @@ class MessagesController < ApplicationController
 
   private
   def set_group
-    @group = messages.group.find(params[:id])
+    @group = Group.find(params[:group_id])
   end
 end
