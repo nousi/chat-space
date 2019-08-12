e4fcd23c50ef6d47a2a9aea7621b33eacd29d0f0
nousi <nousidayo@gmail.com>
2019-08-13 00:17:38 +0900

    Editボタンの稼働完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit]
+  before_action :set_group, only: [:edit,:update]
 
   def new
     @group = Group.new
@@ -17,7 +17,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path, notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
