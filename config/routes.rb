7ee9f24a454d836e44f957ad6746371061327a80
nousi <nousidayo@gmail.com>
2019-08-12 19:52:27 +0900

    Revert "これを修正"
    This reverts commit 697d82895d1480416ebfa7a9d51b19e330f7834f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'groups#index'
-  resources :groups, only: [:new, :create, :edit, :update]do
-    resources :messages
-  end
+  root to: 'messages#index'
+  resources :messages
   resources :users, only: [:edit, :update]
 end
