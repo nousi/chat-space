2b43e56b22332a9e21eb543ea158d16380f40b22
nousi <nousidayo@gmail.com>
2019-08-12 23:47:11 +0900

    Groupの編集機能実装
    Groupの編集機能まで実装しています。
    Editボタンを活用できるように、messagesモデルを作成しています

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index,:create]
   def index
   end
   
   def create
+  end
+
+  private
+  def set_group
+    @group = messages.group.find(params[:id])
   end
 end
