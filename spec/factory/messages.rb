eaeaa649a586f899feb763ec8f1ee99da117f206
nousi <nousidayo@gmail.com>
2019-08-15 19:18:52 +0900

    message_spec.rbを記述

--- spec/factory/messages.rb
+++ spec/factory/messages.rb
@@ -1,3 +1,9 @@
 FactoryBot.define do
   factory :message do
-    
+    text {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/text_image.jpg")}
+    user
+    group
+  end
+end
+
