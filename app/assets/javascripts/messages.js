20ea885844aed72f074c3e452e9484a5d4287d32
nousi <nousidayo@gmail.com>
2019-08-25 21:38:26 +0900

    HTMLのappend以外改善
    messageの非同期通信の基本的な記述は完了

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,21 +1,39 @@
 $(function(){
+  function buildHTML(message){
+    var html = `.message
+                  .message__info.clearfix
+                    %p.message__info_upper_name
+                      ${message.user_name}
+                    %p.message__info_upper_date
+                      ${message.created_at}
+                  - if ${message.text}.present?
+                    %p.message__text
+                      ${message.text}
+                  = image_tag ${message.image_url},　class: 'lower-message__image' if ${message.image}.present?`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
-    e.preventDetafault();
+    e.preventDefault();
     console.log(this)
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    // var href = location.href
+    // var href = '/groups/${message.}/messages'
     $.ajax({
-      url: url;
+      url: location.href,
       type: 'POST',
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-    .done(funciton(data){
-      var html = buildHTML(data);
-      $('').append(html)
+    .done(function(data){
+      console.log('Hello World!!')
+      var html = buildHTML(data)
+      $('.messages').append(html)
       $('.form__message').val('')
+    })
+    .fail(function(){
+      alert('送信に失敗しました。');
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,2 +1,4 @@
-json.array! @messages do |message|
-  json.
+json.user_name @message.user.name
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.text @message.text
+json.image_url @message.image.url
