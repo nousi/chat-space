10732c9befd9653bfb4741597aecf5b7be4cf5c8
nousi <nousidayo@gmail.com>
2019-08-29 21:13:45 +0900

    不要な記述を消去

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,6 @@ class Group < ApplicationRecord
   has_many :members
   has_many :messages
   validates :name, presence: true, uniqueness: true
-  validates :name, presence: true
   
 
   def show_last_message
