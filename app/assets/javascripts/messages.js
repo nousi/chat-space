c494ed7d5903330cd6a07bfc4aee00aed4171e35
nousi <nousidayo@gmail.com>
2019-08-30 17:19:51 +0900

    自動更新：完了

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,39 +1,4 @@
 $(function(){
-  var reloadMessages = function(){
-    var last_message_id = $('.message:last').data('id');
-    $.ajax({
-      url: window.location.href + '/api/messages',
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages){
-      console.log('success');
-      var insetHTML = '';
-    })
-    .fail(function(){
-      console.log('error');
-    });
-  };
-  // var buidMessageHTML = function(message){
-  //   if (message.text && message.image.url){
-  //     var html =`<div class="message" data-id='${message.id}' >
-  //                 <div class="message__info clearfix">
-  //                   <p class="message__info_upper_name">
-  //                     ${message.user_name}
-  //                   </p>
-  //                   <p class="message__info_upper_date">
-  //                     ${message.created_at}
-  //                   </p>
-  //                 </div>
-  //                 <p class="message__text">
-  //                   ${message.text}
-  //                 </p>
-  //                 <img class="lower-message__image" src="${image}">
-  //               </div>`
-  //   }
-  // }
-  
   function buildHTML(message){
     var image = (
       (message.image_url)
@@ -41,7 +6,7 @@ $(function(){
       : ("")
     );
     var html =(
-      (`<div class="message" data-id= "${message.id}" data-user-name= "${message.user.name}" data-image= "${message.image}" data-text="${message.text}" data-created-at= "${message.created_at} >
+      (`<div class="message" data-id= "${message.id}" data-user-name= "${message.user_name}" data-image= "${message.image_url}" data-text="${message.text}" data-created-at= "${message.created_at} >
           <div class="message__info clearfix">
             <p class="message__info_upper_name">
               ${message.user_name}
@@ -85,6 +50,33 @@ $(function(){
       $(".form__submit").removeAttr("disabled");
       });
   })
-  
-  setInterval(reloadMessages, 5000);
+  $(document).on('turbolinks:load',function(){
+    var reloadMessages = function(){
+      if(window.location.href.match(/\/groups\/\d+\/messages/)){
+        var href = 'api/messages#index {:format=>"json"}'
+        var last_message_id = $('.message:last').data('id');
+        $.ajax({
+          // url: window.location.href + '/api/messages',
+          url: href,
+          type: 'GET',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages){
+          console.log('成功')
+          var insertHTML = '';
+            console.log(messages)
+            messages.forEach(function(message){
+              insertHTML = buildHTML(message);
+              $('.messages').append(insertHTML);
+              $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+            });
+        })
+        .fail(function(){
+          alert("自動更新に失敗しました");
+        });
+      };
+    };
+    setInterval(reloadMessages, 5000);
+  });
 });
--- app/views/api/index.json.jbuilder
+++
@@ -1,7 +0,0 @@
-json.arry! @messages do |message|
-  json.text        message.text
-  json.image_url   message.image
-  json.created_at  message.created_at.strftime("%Y/%m/%d %H:%M")
-  json.user_name   message.user.name
-  json.id          message.id
-end
