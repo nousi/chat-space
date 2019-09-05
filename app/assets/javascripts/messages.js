7a8b557c5420df1047dee3a569d3098e65b72163
nousi <nousidayo@gmail.com>
2019-09-05 20:36:24 +0900

    JSがリロードしないと動かない問題の改善

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('turbolinks:load',function(){
   function buildHTML(message){
     var image = (
       (message.image_url)
@@ -50,30 +50,28 @@ $(function(){
       $(".form__submit").removeAttr("disabled");
       });
   })
-  $(document).on('turbolinks:load',function(){
-    var reloadMessages = function(){
-      if(window.location.href.match(/\/groups\/\d+\/messages/)){
-        var href = 'api/messages#index {:format=>"json"}'
-        var last_message_id = $('.message:last').data('id');
-        $.ajax({
-          url: href,
-          type: 'GET',
-          dataType: 'json',
-          data: {id: last_message_id}
-        })
-        .done(function(messages){
-          var insertHTML = '';
-            messages.forEach(function(message){
-              insertHTML = buildHTML(message);
-              $('.messages').append(insertHTML);
-              $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-            });
-        })
-        .fail(function(){
-          alert("自動更新に失敗しました");
-        });
-      };
+  var reloadMessages = function(){
+    if(window.location.href.match(/\/groups\/\d+\/messages/)){
+      var href = 'api/messages#index {:format=>"json"}'
+      var last_message_id = $('.message:last').data('id');
+      $.ajax({
+        url: href,
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        var insertHTML = '';
+          messages.forEach(function(message){
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          });
+      })
+      .fail(function(){
+        alert("自動更新に失敗しました");
+      });
     };
-    setInterval(reloadMessages, 5000);
-  });
+  };
+  setInterval(reloadMessages, 5000);
 });
